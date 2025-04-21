347c5127aaa539f57644455ade343004c647b3f0
iliyas <iliyassipai861@gmail.com>
2025-04-21 09:26:34 +0530

    save life cycel

--- day9-life-sycel/lab-assignment/lab-10/src/App.jsx
+++ day9-life-sycel/lab-assignment/lab-10/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ClassCompo from "./ClassCompo";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //function componet
+  //  task2
+  //  const [count,setCount] = useState(0);
+  //  console.log(count);
+
+  //   useEffect(() =>{
+  //     setTimeout(() => {
+  //        setCount(50)
+  //     },2000)
+  //     console.log(count);
+
+  //   })
 
   return (
     <>
-      
+      {/* task 1
+       Create a class component that fetches data from an API when the component
+      mounts using componentDidMount(). Display the data in the component. */}
+      <ClassCompo />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

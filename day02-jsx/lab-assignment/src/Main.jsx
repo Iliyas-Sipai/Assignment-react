e6cf3e2f2b50de3c4959c18d1f89cc860d115b7a
iliyas <iliyassipai861@gmail.com>
2025-04-21 09:28:34 +0530

    some chnages

--- day02-jsx/lab-assignment/src/Main.jsx
+++ day02-jsx/lab-assignment/src/Main.jsx
@@ -1,22 +1,23 @@
 const Main = () => {
-    const name = "Iliyas";
+  const name = "Iliyas";
 
-    
-    const getUserName = () => {
+  const getUserName = () => {
+    return "Sipaiiliyas@123";
+  };
+  return (
+    <>
+      <h1>Welcome to JSX</h1>
+      JSX (JavaScript XML) is a syntax extension used in React that allows
+      developers to write HTML-like code within JavaScript. It makes creating UI
+      components more intuitive and readable. One powerful feature of JSX is the
+      ability to insert dynamic data using curly braces {}. For example, if you
+      have a variable like const name = "Iliyas"; you can display it in your JSX
+      by writing <h1>Hello, {name}!</h1>. The value inside the curly braces is
+      treated as regular JavaScript, so you can also include expressions like{" "}
+      {5 + 3} or function calls like {getUserName()}. This makes JSX very
+      flexible for building dynamic and interactive user interfaces.
+    </>
+  );
+};
 
-      return "Sipaiiliyas@123"
-}
-    return(
-        <>
-           <h1>Welcome to JSX</h1>
-             JSX (JavaScript XML) is a syntax extension used in React that allows developers to write HTML-like code within JavaScript. 
-             It makes creating UI components more intuitive and readable. One powerful feature of JSX is the ability to insert dynamic data using curly braces {}. 
-             For example, if you have a variable like const name = "Iliyas"; you can display it in your JSX by writing <h1>Hello, {name}!</h1>.
-              The value inside the curly braces is treated as regular JavaScript, 
-              so you can also include expressions like {5 + 3} or function calls like {getUserName()}. 
-              This makes JSX very flexible for building dynamic and interactive user interfaces.
-              </>
-    )
-}
-
-export default Main
+export default Main;
